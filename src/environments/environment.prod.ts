507239cdecfc511c55207764d5fc4002114fd8b0
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-08 17:24:12 -0500

    feat: secret firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAUaQXZlNpo5UkR13QfmvghRuf5KYGJ6GU",
+    authDomain: "rest-api-task-8aeb7.firebaseapp.com",
+    projectId: "rest-api-task-8aeb7",
+    storageBucket: "rest-api-task-8aeb7.firebasestorage.app",
+    messagingSenderId: "890426724417",
+    appId: "1:890426724417:web:078440287c03d4c9f3f6c0",
+    measurementId: "G-CJ3G70M9SR"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAUaQXZlNpo5UkR13QfmvghRuf5KYGJ6GU",
+    authDomain: "rest-api-task-8aeb7.firebaseapp.com",
+    projectId: "rest-api-task-8aeb7",
+    storageBucket: "rest-api-task-8aeb7.firebasestorage.app",
+    messagingSenderId: "890426724417",
+    appId: "1:890426724417:web:078440287c03d4c9f3f6c0",
+    measurementId: "G-CJ3G70M9SR"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
